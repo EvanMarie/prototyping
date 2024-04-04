6cc1b048750f97fd3ee2bab4b85940b7b6f6c519
Evan Marie Carr <evanmariecarr@gmail.com>
2024-04-04 18:07:59 -0400

    trying to figure out which library to use:

--- app/routes/design+/test.tsx
+++ app/routes/design+/test.tsx
@@ -1,6 +1,15 @@
-import CenterFull from "~/components/buildingBlocks/centerFull";
-import { motion } from "framer-motion";
+import ApexChart from "~/components/data-visualization/barChart";
 
-export default function Test() {
-  return <CenterFull>this</CenterFull>;
+export default function ChartPage() {
+  const data = {
+    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    series: [12, 19, 3, 5, 2, 3],
+  };
+
+  return (
+    <div className="container mx-auto py-8">
+      <h1 className="text-2xl font-bold mb-4">Data Visualization</h1>
+      <ApexChart />
+    </div>
+  );
 }
