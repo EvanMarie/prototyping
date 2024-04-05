8f242f9aac99a4336098d3c2dd5fadddf29adf12
Evan Marie Carr <evanmariecarr@gmail.com>
2024-04-05 13:34:55 -0400

    repurposed this repo

--- app/routes/design+/test.tsx
+++ app/routes/design+/test.tsx
@@ -1,12 +1,3 @@
-import { ClientOnly } from "remix-utils/client-only";
-import ChartComponent from "~/components/data-visualization/barChart.client";
-import ChartOne from "~/components/data-visualization/barChart.client";
-import ChartWrapper from "~/components/data-visualization/chartWrapper";
-
-export default function ChartExample() {
-  return (
-    <ChartWrapper>
-      <ChartComponent />
-    </ChartWrapper>
-  );
+export default function Test() {
+  return <div>Test</div>;
 }
